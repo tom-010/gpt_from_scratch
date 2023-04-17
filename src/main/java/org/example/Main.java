3ad39e551b253ea9a7725ebd661f352d757607f3
Thomas Deniffel <tdeniffel@gmail.com>
2023-04-17 18:26:21 +0200

    missing code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,81 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-class Encoder {
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void main3(String[] args) throws MalformedModelException, IOException, ParseException, TranslateException {
+        Path modelDir = Paths.get("./");
+        Model model = Model.newInstance("gpt_gpu");
+        model.load(modelDir);
+        Predictor<List<Integer>, Integer> predictor = model.newPredictor(new GptTranslator());
+        predictor.predict(List.of(0));
+    }
+
+
+    public static void main2(String[] args) throws IOException, MalformedModelException, TranslateException {
+        DataSource ds = new DataSource("input.txt");
+        ds.read();
+        var encoder = ds.encoder;
+        System.out.println(encoder.getAlphabetString());
+
+        Path modelDir = Paths.get("./");
+        Model model = Model.newInstance("gpt");
+        model.load(modelDir);
+
+        Predictor<List<Integer>, Integer> predictor = model.newPredictor(new GptTranslator());
+
+        int contextSize = 64;
+        List<Integer> context = new LinkedList<Integer>();
+        context.add(0); // initial token
+
+        for(int i=0; i<500; i++) {
+            int nextToken = predictor.predict(List.of(50));
+            context.add(nextToken);
+            while(context.size() > contextSize) {
+                context.remove(0);
+            }
+            System.out.print(encoder.decode(nextToken));
+        }
+        System.out.println();
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class Encoder2 {
 
     Map<Integer, Integer> codePointToEncoding = new HashMap<>();
     Map<Integer, Integer> encodingToCodePoint = new HashMap<>();
@@ -81,7 +155,7 @@ class Sample {
 
 class DataSource {
 
-    Encoder encoder;
+    Encoder2 encoder;
     String path;
 
     List<Integer> encoded;
@@ -104,7 +178,7 @@ class DataSource {
 
     void read() throws IOException {
         String content = readFile();
-        encoder = new Encoder();
+        encoder = new Encoder2();
         encoder.init(content);
         encoded = encoder.encode(content);
         splitData();
@@ -149,25 +223,8 @@ class DataSource {
                 list.subList(position+1, position + contextLength + 1));
     }
 
-    void store(String filename) throws IOException {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("trainingData", trainingData);
-        jsonObject.put("testData", testData);
-        jsonObject.put("codePointToEncoding", encoder.codePointToEncoding);
-        jsonObject.put("encodingToCodePoint", encoder.encodingToCodePoint);
-        FileWriter file = new FileWriter(filename);
-        jsonObject.writeJSONString(file);
-        file.close();
-    }
 
-    static DataSource fromCache(String filename) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        FileReader file = new FileReader(filename);
-        var parsed = (JsonObject) parser.parse(file);
-        var trainingData = parsed.get("trainingData");
-        System.out.println(trainingData);
-        return new DataSource();
-    }
+
 }
 
 class GptTranslator implements Translator<List<Integer>, Integer> {
@@ -198,43 +255,214 @@ class BashTranslator {
 
 }
 
-public class Main {
 
-    public static void main(String[] args) throws MalformedModelException, IOException, ParseException, TranslateException {
-        Path modelDir = Paths.get("./");
-        Model model = Model.newInstance("gpt_gpu");
-        model.load(modelDir);
-        Predictor<List<Integer>, Integer> predictor = model.newPredictor(new GptTranslator());
-        predictor.predict(List.of(0));
+
+
+
+class Sample2 {
+    List<Integer> input;
+    List<Integer> expected;
+
+    Sample2(List<Integer> input, List<Integer> expected) {
+        this.input = input;
+        this.expected = expected;
     }
 
+    int size() {
+        return input.size();
+    }
 
-    public static void main2(String[] args) throws IOException, MalformedModelException, TranslateException {
-        DataSource ds = new DataSource("input.txt");
-        ds.read();
-        var encoder = ds.encoder;
-        System.out.println(encoder.getAlphabetString());
+    @Override
+    public String toString() {
+        return "Sample{" +
+                "input=" + input +
+                ", expected=" + expected +
+                '}';
+    }
+}
 
-        Path modelDir = Paths.get("./");
-        Model model = Model.newInstance("gpt");
-        model.load(modelDir);
+class DataSource2 {
 
-        Predictor<List<Integer>, Integer> predictor = model.newPredictor(new GptTranslator());
+    Encoder2 encoder;
+    String path;
 
-        int contextSize = 64;
-        List<Integer> context = new LinkedList<Integer>();
-        context.add(0); // initial token
+    List<Integer> encoded;
 
-        for(int i=0; i<500; i++) {
-            int nextToken = predictor.predict(List.of(50));
-            context.add(nextToken);
-            while(context.size() > contextSize) {
-                context.remove(0);
+    List<Integer> trainingData;
+
+    List<Integer> testData;
+
+    Random ran = new Random();
+
+
+    DataSource2(String path) {
+        this.path = path;
+    }
+
+    void read() throws IOException {
+        String content = readFile();
+        encoder = new Encoder2();
+        encoder.init(content);
+        encoded = encoder.encode(content);
+        splitData();
+    }
+
+    private String readFile() throws IOException {
+        File file = new File(path);
+        InputStream inputStream = new FileInputStream(file);
+
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+        try (BufferedReader br
+                     = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                resultStringBuilder.append(line).append("\n");
             }
-            System.out.print(encoder.decode(nextToken));
+        }
+        return resultStringBuilder.toString();
+    }
+
+    void splitData() {
+        int i = (int) (0.9 * encoded.size());
+        trainingData = encoded.subList(0, i);
+        testData = encoded.subList(i, encoded.size());
+    }
+
+    Sample getTrainingSample(int contextLength) {
+        return drawFromList(trainingData, contextLength);
+    }
+
+    Sample getTestSample(int contextLength) {
+        return drawFromList(trainingData, contextLength);
+    }
+
+    private Sample drawFromList(List<Integer> list, int contextLength) {
+        int max = list.size() - (contextLength+1);
+        int position = ran.nextInt(max);
+
+        return new Sample(
+                list.subList(position, position + contextLength),
+                list.subList(position+1, position + contextLength + 1));
+    }
+}
+
+class Counter2 {
+
+    Map<Integer, Integer> tally = new HashMap<>();
+    List<Integer> elements = new LinkedList<>();
+
+    private Random ran = new Random();
+
+    void add(Integer i) {
+        if(!tally.containsKey(i)) {
+            tally.put(i, 0);
+        }
+        tally.put(i, tally.get(i) + 1);
+        elements.add(i);
+    }
+
+    Integer draw() {
+        return elements.get(ran.nextInt(elements.size()));
+    }
+
+    public void showTally(Encoder2 encoder) {
+        var keys = tally.keySet().stream().sorted().toList();
+        for(int key : keys) {
+            System.out.print(encoder.decode(List.of(key)));
+            System.out.print("=");
+            System.out.print(tally.get(key) + "; ");
+
         }
         System.out.println();
+
+        System.out.println(encoder.decode(elements));
     }
 
 
 }
+
+class BigramLanguageModel2 {
+
+    private Random ran = new Random();
+
+    Map<Integer, Counter2> table = new HashMap<>();
+
+    void addToTable(int input, int expected) {
+        if(!table.containsKey(input)) {
+            table.put(input, new Counter2());
+        }
+        table.get(input).add(expected);
+    }
+
+    void fit(Sample sample) {
+        for(int i=0; i<sample.size(); i++) {
+            addToTable(sample.input.get(i), sample.expected.get(i));
+        }
+    }
+
+    int nextToken(List<Integer> context) {
+        int current = context.get(context.size()-1);
+        return table.get(current).draw();
+
+    }
+
+    private int randomToken() {
+        var options = table.keySet().stream().toList();
+        var idx = ran.nextInt(options.size());
+
+        return options.get(idx);
+    }
+
+    public void showTable(Encoder2 encoder) {
+        var keys = table.keySet().stream().sorted().toList();
+        for(int key : keys) {
+            System.out.println("=====");
+            System.out.println(encoder.decode(List.of(key)));
+            table.get(key).showTally(encoder);
+        }
+    }
+
+}
+
+
+class NGramLanguageModel2 {
+
+    private Random ran = new Random();
+    int n;
+
+    NGramLanguageModel2(int n) {
+        this.n = n;
+    }
+
+    Map<List<Integer>, Counter2> table = new HashMap<>();
+
+    void addToTable(List<Integer> input, int expected) {
+        if(!table.containsKey(input)) {
+            table.put(input, new Counter2());
+        }
+        table.get(input).add(expected);
+    }
+
+    void fit(Sample sample) {
+        for(int i=1; i<sample.size(); i++) {
+            int start = i - n;
+            if(start < 0) {
+                start = 0;
+            }
+            addToTable(sample.input.subList(start, i), sample.expected.get(i));
+        }
+    }
+
+    int nextToken(List<Integer> context) {
+        int start = context.size() - n;
+        if(start < 0) {
+            start = 0;
+        }
+
+        var current = context.subList(start, context.size());
+        return table.get(current).draw();
+
+    }
+}
+
